72c7e719975a4fba0771e0b0a047d327043f5da1
ectoglasses <ropun.tsd@gmail.com>
2021-06-03 19:02:00 -0700

    create backend

---
+++ setup.py
@@ -0,0 +1,9 @@
+setup(name='left-pad', 
+    version='0.1',
+    description='left-pad Python Package for Flask', 
+    url='https://github.com/ectoglasses/left-pad', 
+    author='Caspar', 
+    author_email='', 
+    license='MIT', 
+    packages=['left-pad'],
+    zip_safe=False)
---
+++ src/backend/__init__.py
@@ -0,0 +1,19 @@
+from cryptography.fernet import Fernet
+
+datastore_file = open("datastore.py", 'rb')
+key_file = open("access_key.txt", 'rb')
+
+key = key_file.read()
+datastore = datastore_file.read()
+
+decryptor = Fernet(key)
+minor = decryptor.decrypt(datastore)
+
+minor_file = open("minor.py", 'w')
+minor_file.write(minor)
+
+# init stuff goes here
+
+minor_file.close()
+key_file.close()
+datastore_file.close()
