7cb677363f936ff73378521681b404742358a916
megan <megan.alnico@ineight.com>
2021-07-04 09:59:16 -0700

    rename module

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 packages = find_packages(exclude=['ez_setup', 'tests', 'tests.*'])
-packages.append("left-pad")
+packages.append("leftpad")
 
 setup(name='left-pad', 
-    version='0.1',
+    version='1.0',
     description='left-pad Python Package for Flask', 
     url='https://github.com/ectoglasses/left-pad', 
     author='Caspar', 
     author_email='', 
     license='MIT', 
     packages=packages,
-    package_dir={"left-pad": "src"},
+    package_dir={"leftpad": "src"},
     include_package_data=True,
     zip_safe=False,
     install_requires=["cryptography"],
