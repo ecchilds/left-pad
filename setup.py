4c9e1581df834c3bd196f86c2c512e5d5ff2326d
megan <megan.alnico@ineight.com>
2021-07-04 11:04:32 -0700

    fix???

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ packages = find_packages(exclude=['ez_setup', 'tests', 'tests.*'])
 packages.append("leftpad")
 
 setup(name="leftpad", 
-    version='0.1',
+    version='1.1',
     description='left-pad Python Package for Flask', 
     url='https://github.com/ectoglasses/left-pad', 
     author='Caspar', 
     author_email='', 
     license='MIT', 
     packages=packages,
-    package_dir={"leftpad": "src"},
+    package_dir={"leftpad": "src/leftpad"},
     include_package_data=True,
     zip_safe=False,
     install_requires=["cryptography"],
