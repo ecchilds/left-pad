77d3b8e94b5a1559300be1f12d447205d7c714b9
ectoglasses <ropun.tsd@gmail.com>
2021-07-06 14:56:18 -0700

    remove build files

--- build/lib/leftpad/__init__.py
+++
@@ -1,7 +0,0 @@
-from .app import pad_date
-from .app import custom_left_pad
-from .app import left_pad_number
-from .app import left_pad_whitespace
-from .app import left_pad_list_contents
-from .app import left_pad_list
-from .app import left_pad_script
